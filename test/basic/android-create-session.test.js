50088ff9c6e0146f0f2c76a96300ed27c7cab2d9
tarpan <tarpan@infovinity.net>
2019-09-04 16:44:41 +0530

    Developed tc_auto_reg_03

--- test/basic/android-create-session.test.js
+++ test/basic/android-create-session.test.js
@@ -11,6 +11,13 @@ let today = new Date();
 let time = today.getDate()+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 console.log("Build:-"+time);
 
+let mobileNummber = Math.floor(Math.random() * 9000000000) + 1000000000;
+let firstnamevar = 'testFName';
+let lastnamevar = 'testLName'
+let emailvar = 'test@yop.com'
+let moberroemsg ='Not a valid phone number.'
+let passerrormsg= 'Should be 6 characters or more'
+
 function isAlertPresent(driver){ 
 	try 
 	{ 
@@ -23,7 +30,7 @@ function isAlertPresent(driver){
 	}   // catch 
 }
 
-describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', function () {
+describe('TC_Auto_Reg_02 : Launch Karma APP & Make a successful Registration', function () {
 	let driver;
 	let allPassed = true;
 
@@ -32,7 +39,7 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 		allPassed = allPassed && (this.currentTest.state === 'passed');
 	});
   
-	it('should launch karma application successfully.', async function () {
+	it('TC_Reg_01 : should launch karma application successfully.', async function () {
 		try {
 			
 			
@@ -45,13 +52,13 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 			*/
 			
 			const browserStackCaps = {
-			  'browserstack.user' : 'karmademo1',
-			  'browserstack.key' : 'yavqKhpyfJ3KmeCHpswq',
-			  'build' : 'Karma [Android]'+time,
-			  'name': 'Launch Karma APP & Make a successful Registration',
+			  'browserstack.user' : 'prakashkhandelwa4',
+			  'browserstack.key' : 'eswAGeNrz4JtEx3TtKNT',
+			  'build' : 'Karma [Android]-'+time,
+			  'name': 'TC_Auto_Reg_02 : Launch Karma APP & Make a successful Registration',
 			  'device' : 'Google Nexus 6',
 			  'platformVersion' : '6.0',
-			  'app' : 'bs://9a4fab1ad5dc487503a5de3d799782ac3d3996a9',
+			  'app' : 'bs://b8d7ccc4bc77ba8b89f4c9801f884659c865b89e',
 			  'autoGrantPermissions' : true,
 			  'browserstack.debug' : true,
 			  'unicodeKeyboard': true
@@ -98,8 +105,7 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 		assert.equal(await loginButtonOnRegistrationPage.isDisplayed(), true,"LOGIN button displayed Successfully.");
 	});
 
-
-
+	/*
 	it('TC_Reg_04 : Verify default country flag/country code from phone number text field.', async function () {
 		
 		const code = await driver.elementByXPath('//android.view.ViewGroup[@content-desc=\'phoneContainer-testId\']/android.view.ViewGroup/android.widget.TextView');
@@ -119,7 +125,7 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 		
 	});
 
-		it('TC_Reg_06, TC_Reg_07, TC_Reg_08 : Verify search country functionality.', async function () {
+	it('TC_Reg_06, TC_Reg_07, TC_Reg_08 : Verify search country functionality.', async function () {
 		
 		const countryFlag = await driver.elementByXPath('//android.view.ViewGroup[@content-desc=\"phoneContainer-testId\"]/android.view.ViewGroup/android.widget.ImageView');
 		await countryFlag.click();
@@ -138,14 +144,11 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
         console.log("Clicked on United Kingdom Country Option.");
 
 		
-	});
+	});*/
 
-	
-	
 	it('TC_Reg_23 :verify "Register" button functionality with valid details.', async function () {		
-	
-		let mobileNummber = Math.floor(Math.random() * 9000000000) + 1000000000;
-	
+
+		await driver.setImplicitWaitTimeout(1000);
 		const mobileNo = await driver.elementByAccessibilityId('phone-testId');
 		await mobileNo.sendKeys(mobileNummber);
 		console.log("Entered Mobile number : "+mobileNummber);
@@ -199,12 +202,12 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 	it('TC_Reg_33,TC_Reg_54 :  verify details on personal details screen.', async function () {	
 	
 	const firstNameField =  await driver.elementByAccessibilityId("firstName-testId");
-	await firstNameField.sendKeys('testFName');
+	await firstNameField.sendKeys(firstnamevar);
 	console.log("Entered firstName.");
 	
 	await driver.setImplicitWaitTimeout(1000);
 	const lastNameField= await driver.elementByAccessibilityId("lastName-testId");
-	await lastNameField.sendKeys('testLName');
+	await lastNameField.sendKeys(lastnamevar);
 	console.log("Entered lastName.");
 	
 	const dobField =  await driver.elementByAccessibilityId("birthDateContainer-testId");
@@ -217,7 +220,7 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 	
 	await driver.setImplicitWaitTimeout(1000);
 	const emailField = await driver.elementByAccessibilityId("emailId-testId");
-	await emailField.sendKeys('test@yop.com');
+	await emailField.sendKeys(emailvar);
 	console.log("Entered EmailID.");
 	
 	await driver.setImplicitWaitTimeout(1000);
@@ -266,7 +269,7 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 	await driver.setImplicitWaitTimeout(1000);
 	const allowButton = await driver.elementByAccessibilityId("allowBtn-testId");
 	const skipButton = await driver.elementByXPath("//android.widget.TextView[@text='skip']");
-	assert.equal(await allowButton.isDisplayed() && await skipButton.isDisplayed(),true,"Successfully allow and skip button displayed.")
+	assert.equal(await allowButton.isDisplayed() && await skipButton.isDisplayed(),true,"Successfully allow and skip button displayed.");
 	
   });
   
@@ -278,13 +281,12 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
     
 	const step3and4Text = await driver.elementByXPath("//android.widget.TextView[@text='Step 3 of 4: Quick ID check']");
 	const takeaSelfie = await driver.elementByAccessibilityId("startPictureBtn-testId");
-	assert.equal(await step3and4Text.isDisplayed() && await takeaSelfie.isDisplayed(),true,"Successfully step3and4 Text and takeaSelfie button displayed.")
+	assert.equal(await step3and4Text.isDisplayed() && await takeaSelfie.isDisplayed(),true,"Successfully step3and4 Text and takeaSelfie button displayed.");
 	
   });
-  
-  
+
   it('TC_Reg_74, TC_Reg_77 : Verify  TAKE A SELFIE button functionality from verify your identity screen.', async function () {
-    const takeaSelfie = await driver.elementByAccessibilityId("startPictureBtn-testId")
+    const takeaSelfie = await driver.elementByAccessibilityId("startPictureBtn-testId");
 	await takeaSelfie.click();	
 	console.log("Taped on take selfie button.");
 	await driver.setImplicitWaitTimeout(1000);
@@ -292,21 +294,165 @@ describe('TC_Reg_01 : Launch Karma APP & Make a successful Registration', functi
 	const capture = await driver.elementByAccessibilityId("captureSelfie-testId");
 	await capture.click();	
 	console.log("Taped on Capture button.");
-	await driver.setImplicitWaitTimeout(1000);
+	await driver.setImplicitWaitTimeout(4000);
   
   });
   
    it('TC_Reg_78 : verify  DONE button functionality from finished screen.', async function () {
-	await driver.setImplicitWaitTimeout(2000);
+	await driver.setImplicitWaitTimeout(5000);
 	const done = await driver.elementByAccessibilityId("doneBtn-testId");
 	await done.click();	
 	console.log("Taped on Done button.");
-	
-	await driver.setImplicitWaitTimeout(1000);
-		
-	await driver.quit();
-			 
-		
+	await driver.setImplicitWaitTimeout(2000);
+
    });
-  
+
+   it('Verify apply screen.', async function () {
+
+		const apply = await  driver.elementByAccessibilityId("Apply, tab, 1 of 5");
+	    const dashboard = await  driver.elementByAccessibilityId("Dashboard, tab, 2 of 5");
+	    const cards = await  driver.elementByAccessibilityId("Cards, tab, 3 of 5");
+	    const profile = await  driver.elementByAccessibilityId("Profile, tab, 4 of 5");
+	    const more = await  driver.elementByAccessibilityId("More, tab, 5 of 5");
+		const sport = await driver.elementByXPath("//android.widget.TextView[@text='Sports']");
+        assert.equal(await apply.isDisplayed() && await dashboard.isDisplayed() && await cards.isDisplayed() && await profile.isDisplayed() && await more.isDisplayed() && await sport.isDisplayed() ,true,"all elements displayed successfully on apply screen.");
+        await driver.setImplicitWaitTimeout(2000);
+   });
+
+	it('Verify user profile screen.', async function () {
+
+		 const profile = await  driver.elementByAccessibilityId("Profile, tab, 4 of 5");
+		 await profile.click();
+		await driver.setImplicitWaitTimeout(2000);
+
+		const firstname = await driver.elementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.EditText");
+		const lastname = await driver.elementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.EditText");
+		const dob = await driver.elementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[5]/android.widget.EditText");
+		const email = await driver.elementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]/android.widget.EditText");
+		const mobile = await driver.elementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[7]/android.widget.EditText");
+
+		let fn = await firstname.text();
+		console.log("Actual First Name : "+fn);
+		let ln = await lastname.text();
+		console.log("Actual Last Name : "+ln);
+		let emailID = await email.text();
+		console.log("Actual Email ID : "+emailID);
+		let mobilNo = await mobile.text();
+		console.log("Actual Mobile : "+mobilNo);
+		let mob = '+44'+mobileNummber;
+
+		/*fn.equal(firstnamevar);
+ 		ln.equal(lastnamevar);
+ 		emailID.equal(emailvar);*/
+
+		//assert.equal(fn.equals(firstnamevar) &&  ln.equals(lastnamevar) &&  emailID.equals(emailvar) &&  mobilNo.equals('+44'+mobileNummber), true, "all elements displayed successfully on user profile screen.");
+		assert.equal(fn,firstnamevar,"First Name Matched successfully");
+		assert.equal(ln,lastnamevar,"Last Name Matched successfully");
+		assert.equal(emailID,emailvar,"Email Id Matched successfully");
+		assert.equal(mobilNo,mob,"Mobile Number Matched successfully");
+
+		await driver.setImplicitWaitTimeout(1000);
+		await driver.quit();
+	});
+
 });
+
+describe('TC_Auto_Reg_03 : To verify Register page validations scenarios.', function () {
+	let driver;
+	let allPassed = true;
+
+	afterEach(function () {
+		// keep track of whether all the tests have passed, since mocha does not do this
+		allPassed = allPassed && (this.currentTest.state === 'passed');
+	});
+
+	it('should launch karma application successfully.', async function () {
+		try {
+
+
+			driver = await wd.promiseChainRemote(serverConfig)
+			/*
+                await driver.init({
+                    ...androidCaps,
+                    app: androidTestApp,
+                });
+                */
+
+			const browserStackCaps = {
+				'browserstack.user': 'prakashkhandelwa4',
+				'browserstack.key': 'eswAGeNrz4JtEx3TtKNT',
+				'build': 'Karma [Android]-' + time,
+				'name': 'TC_Auto_Reg_03 : To verify Register page validations scenarios.',
+				'device': 'Google Nexus 6',
+				'platformVersion': '6.0',
+				'app': 'bs://b8d7ccc4bc77ba8b89f4c9801f884659c865b89e',
+				'autoGrantPermissions': true,
+				'browserstack.debug': true,
+				'unicodeKeyboard': true
+			};
+
+			driver = wd.promiseRemote('http://hub-cloud.browserstack.com/wd/hub');
+			await driver.init(browserStackCaps);
+			await driver.setImplicitWaitTimeout(10000);
+
+
+			// Check that we're running the Karma app by checking package and activity
+			//const activity = await driver.getCurrentActivity();
+			//const pkg = await driver.getCurrentPackage();
+			//console.log(activity);
+
+			const registerBtn = await driver.elementByAccessibilityId("registerBtn-testId");
+			const btnLogin = await driver.elementByAccessibilityId("loginBtn-testId");
+
+			assert.equal(await registerBtn.isDisplayed() && await btnLogin.isDisplayed(), true, "Application launch successfully.");
+		} finally {
+			// Quit the session, no matter what happens
+			// await driver.quit();
+		}
+	});
+
+	it('TC_Reg_02 : Verify "REGISTER" button functionality', async function () {
+		await driver.setImplicitWaitTimeout(5000);
+
+		const register = await driver.elementByAccessibilityId("registerBtn-testId");
+		await register.click();
+		console.log("Clicked on 'Register' button.");
+
+		const mobileNo = await driver.elementByAccessibilityId("phone-testId");
+		const pswd = await driver.elementByAccessibilityId("password-testId");
+		const checkBox = await driver.elementByAccessibilityId("aggrementCheckbox-testId")
+
+		assert.equal(await mobileNo.isDisplayed() && await pswd.isDisplayed() && await checkBox.isDisplayed(), true, "Successfully Redirected to Register page.");
+	});
+
+	it('TC_Reg_11 : To verify validation message for less then 10 digit phone number.', async function () {
+
+		await driver.setImplicitWaitTimeout(1000);
+
+		const mobileNo2 = await driver.elementByAccessibilityId('phone-testId');
+		await mobileNo2.sendKeys('998865');
+		console.log("Entered Mobile number : "+mobileNummber);
+		const mobileNoerror = await driver.elementByXPath("//android.widget.TextView[@text='Not a valid phone number.']");
+
+		let mobileNoerrormsg = mobileNoerror.text();
+
+		assert.equal(mobileNoerrormsg,moberroemsg,"validation message displayed successfully");
+
+	});
+
+	it('TC_Reg_13 : To verify validation message for less than 6 characters in password field.', async function () {
+
+		const pswd = await driver.elementByAccessibilityId("password-testId");
+		await pswd.sendKeys('9ew5');
+		console.log("Entered  Password");
+
+		const passworderror = await driver.elementByXPath("//android.widget.TextView[@text='Should be 6 characters or more']");
+		let actualpassworderrormsg = passworderror.text();
+
+		assert.equal(actualpassworderrormsg,passerrormsg,"validation message displayed successfully");
+
+		await driver.quit();
+	});
+
+});
+
